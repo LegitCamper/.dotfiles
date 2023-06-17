614dd798d61ce4178c6028391e3a521a6450324d
sawyer bristol <sawyerbristol@gmail.com>
2023-06-16 23:15:49 -0600

    fix services

--- nix/system/configuration.nix
+++ nix/system/configuration.nix
@@ -2,7 +2,11 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, lib, ...}: {
+{ config, pkgs, lib, ...}: 
+
+  {
+  nixpkgs.config.allowUnfree = true;
+  
   programs.adb.enable = true;
   programs.hyprland.enable = true;
 
