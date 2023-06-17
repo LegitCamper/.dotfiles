0f05831b4585cba982124486c7f73fcc21a4524b
sawyer bristol <sawyerbristol@gmail.com>
2023-06-17 15:51:39 -0600

    should update repo now too

--- nix/.update.sh
+++ nix/.update.sh
@@ -2,6 +2,10 @@
 
 cd ~/projects/.dotfiles/nix
 
+git rebase .
+
+git pull
+
 nixos-rebuild --use-remote-sudo switch && 
 nix flake update --extra-experimental-features flakes --extra-experimental-features nix-command && 
 nixos-rebuild --use-remote-sudo switch --impure --flake .#nixos-desktop
