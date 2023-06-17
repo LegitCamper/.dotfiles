c51012630cc1db042855a133e92ad918d71039ec
sawyer bristol <sawyerbristol@gmail.com>
2023-06-17 16:52:04 -0600

    fix more errors

--- nix/.update.sh
+++ nix/.update.sh
@@ -8,8 +8,8 @@ git pull
 
 rm -rm flake.lock
 
-nixos-rebuild --use-remote-sudo switch  
+sudo nixos-rebuild switch  
 nix flake update --extra-experimental-features flakes --extra-experimental-features nix-command  
-nixos-rebuild --use-remote-sudo switch --impure --flake .#nixos-desktop
+sudo nixos-rebuild switch --impure --flake .#nixos-desktop
 
 cd -
