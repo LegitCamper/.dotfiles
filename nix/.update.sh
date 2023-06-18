6017ebf3536359534e5f155180009ff7e90c4ebe
sawyer bristol <sawyerbristol@gmail.com>
2023-06-17 21:31:39 -0600

    fix update script

--- nix/.update.sh
+++ nix/.update.sh
@@ -8,8 +8,8 @@ git pull
 
 rm -rm flake.lock
 
-sudo nixos-rebuild switch  
-nix flake update --extra-experimental-features flakes --extra-experimental-features nix-command  
-sudo nixos-rebuild switch --impure --flake .#nixos-desktop
+# sudo nixos-rebuild switch  
+# nix flake update --extra-experimental-features flakes --extra-experimental-features nix-command  
+sudo bash -c "nixos-rebuild switch --impure --flake .#nixos-desktop"
 
 cd -
