32695ea2cbc0507ec8df3e677f78fbc764661a50
sawyer bristol <sawyerbristol@gmail.com>
2023-06-18 12:06:33 -0600

    adding gtk config to nix

--- nix/home.nix
+++ nix/home.nix
@@ -82,6 +82,15 @@
     starship
   ];
 
+  # gtk configuration
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Catppuccin-Macchiato-Standard-Mauve-Dark";
+      package = pkgs.catppuccin-gtk;
+    };
+  };
+
   # This value determines the home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new home Manager release introduces backwards
