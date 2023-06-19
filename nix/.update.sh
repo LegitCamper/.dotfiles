5c269789a862ffd45b4a8a6477b805f7b01ab754
sawyer bristol <sawyerbristol@gmail.com>
2023-06-19 12:47:38 -0600

    remove uneeded generations

--- nix/.update.sh
+++ nix/.update.sh
@@ -13,4 +13,8 @@ git pull
 # nix flake update --extra-experimental-features flakes --extra-experimental-features nix-command  
 sudo nixos-rebuild switch --upgrade --impure --flake .#nixos-desktop
 
+nix-env --delete-generations 14d
+
+nix-store --gc
+
 cd -
