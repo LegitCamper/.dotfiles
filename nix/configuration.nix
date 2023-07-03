0e8e984714ac679d6b9494a1a6a0c0d1e0e7465d
sawyer bristol <sawyerbristol@gmail.com>
2023-07-03 14:18:35 -0600

    adding locals

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -47,6 +47,17 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
 
   sound.enable = true;
   hardware = {
@@ -119,7 +130,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.sawyer = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "input" "disk" "wireshark" ];
+    extraGroups = [ "networkmanager" "wheel" "input" "disk" "wireshark" ];
   };
 
   # List packages installed in system profile. To search, run:
