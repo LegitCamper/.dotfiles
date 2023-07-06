0551641c0f07da04731f2a239bcd5e91a148bd66
sawyer bristol <sawyerbristol@gmail.com>
2023-07-05 18:46:44 -0600

    add hardware configuration

--- nix/hosts/nixos-laptop.nix
+++ nix/hosts/nixos-laptop.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ lib, config, pkgs, modulesPath, ... }: {
 
   # Use the systemd-boot EFI boot loader.
   boot.loader = {
@@ -48,4 +48,36 @@
       videoDrivers = [ "amdgpu" ];
     };
   };
+
+  # from hardware-configuration.nix
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+
+  boot.initrd.availableKernelModules =
+    [ "nvme" "xhci_pci" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/049138bd-02ad-4dce-856a-da4f1212c858";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/5AB4-FB64";
+    fsType = "vfat";
+  };
+
+  swapDevices = [ ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
