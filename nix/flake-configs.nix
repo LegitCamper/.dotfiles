f428492fbe23bafc538f5b6564477acc3ca44e47
sawyer bristol <sawyerbristol@gmail.com>
2023-08-19 13:28:30 -0600

    remove package configs

--- nix/flake-configs.nix
+++ nix/flake-configs.nix
@@ -1,11 +1,11 @@
 { config, lib, packages, pkgs, inputs', ... }: {
   environment.systemPackages = [
-    inputs'.anyrun.packages.anyrun
-    inputs'.anyrun.packages.applications
-    inputs'.anyrun.packages.dictionary
-    inputs'.anyrun.packages.rink
+    # inputs'.anyrun.packages.anyrun
+    # inputs'.anyrun.packages.applications
+    # inputs'.anyrun.packages.dictionary
+    # inputs'.anyrun.packages.rink
 
-    inputs'.ags.packages.default
+    # inputs'.ags.packages.default
 
     inputs'.nix-gaming.packages.proton-ge
     inputs'.nix-gaming.packages.wine-ge
