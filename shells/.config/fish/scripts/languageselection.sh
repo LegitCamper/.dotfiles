080c6e63a0b0c0454e9ea60d0042e2892833b596
sawyer bristol <sawyerbristol@gmail.com>
2023-11-05 12:30:23 -0700

    fix dev alias

--- shells/.config/fish/scripts/languageselection.sh
+++ shells/.config/fish/scripts/languageselection.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-devshells=$(echo ~/projects/.dotfiles/nix/devshells/* $(pwd))
-selected=$(echo $devshells| tr " " "\n" |
+devshells=$(echo ~/projects/.nix-config/devshells/* $(pwd))
+selected=$(echo $devshells | tr " " "\n" |
 	fzf -i --header-first \
 		--header="Select your language layout above.")
 
