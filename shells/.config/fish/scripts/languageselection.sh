92fd79c6a3f89a2d069c070bd3c256e59aa8db7c
sawyer bristol <sawyerbristol@gmail.com>
2023-11-05 15:00:00 -0700

    ?

--- shells/.config/fish/scripts/languageselection.sh
+++ shells/.config/fish/scripts/languageselection.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-devshells=$(echo ~/projects/.nix-config/devshells/* $(pwd))
+devshells=$(echo $(pwd))
 selected=$(echo $devshells | tr " " "\n" |
 	fzf -i --header-first \
 		--header="Select your language layout above.")
