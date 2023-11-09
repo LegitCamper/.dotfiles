bd620d8c9e412e42198a74b001e231bda722fe49
sawyer bristol <sawyerbristol@gmail.com>
2023-11-09 14:36:10 -0700

    updating stow

--- .stow.sh
+++ .stow.sh
@@ -2,10 +2,8 @@
 
 cd ~/projects/.dotfiles
 
-rm -rf ~/.config/.bashrc ~/.config/fish ~/.wakatime*
-
 stow -t ~ --adopt -S terminals editors shells hyprlandwm
 
-git restore .
+# git restore .
 
 cd -
