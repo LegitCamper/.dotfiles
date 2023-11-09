aac5a44df13e8c690e1a763d8af6824bfd069d69
sawyer bristol <sawyerbristol@gmail.com>
2023-11-09 14:24:39 -0700

    remove unused wm's

--- qtilewm/.config/qtile/autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# EXTRAS
-xrandr -s 1.0x1.0
-lxsession &
-picom --experimental-backend &
-#megasync &
-dunst &
-nitrogen --restore &
--- qtilewm/.config/qtile/settings/widgets.py
+++
@@ -1,7 +0,0 @@
-from .colors import colors
-
-widget_defaults = dict(
-    fontsize = 12,
-    padding = 2,
-)
-extension_defaults = widget_defaults.copy()
