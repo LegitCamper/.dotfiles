4291b72022c92035227721b58455fe7900f5b57b
sawyer bristol <sawyerbristol@gmail.com>
2024-01-21 15:22:47 -0700

    fixing dev

--- shells/.config/fish/functions/dev.fish
+++ shells/.config/fish/functions/dev.fish
@@ -2,12 +2,12 @@
 
 function dev -d "Searches for project folders"
     set SEARCH_PATH $(bash ~/.config/fish/scripts/projectselection.sh)
-    set LANGUAGE_SELECTION $(bash ~/.config/fish/scripts/languageselection.sh $SEARCH_PATH)
+    # set LANGUAGE_SELECTION $(bash ~/.config/fish/scripts/languageselection.sh $SEARCH_PATH)
     set RANDOM $(shuf -i 1-1000000 -n 1)
 
     cd $SEARCH_PATH 
-    eval "$LANGUAGE_SELECTION -c \
-        zellij -s dev-env-$RANDOM -l ~/.config/zellij/layouts/development.kdl \
-        options --on-force-close detach --simplified-ui false"
+    # eval "$LANGUAGE_SELECTION -c \
+    zellij -s dev-env-$RANDOM -l ~/.config/zellij/layouts/development.kdl \
+    options --on-force-close detach --simplified-ui false #"
     cd -
 end
