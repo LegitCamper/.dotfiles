4e81cf2f45281c9cf77cd5de2115b91b8d49497b
sawyer bristol <sawyerbristol@gmail.com>
2024-12-17 20:00:24 -0700

    zellij

--- dots/.config/zellij/start.sh
+++ dots/.config/zellij/start.sh
@@ -1,6 +1,10 @@
 #!/bin/usr/bash
 
+zellijname=$(shuf -i 1-100000 -n 1)
+
 while true; do
   cd ~/projects/$(eza -a ~/projects/ | fzf --margin 30% --header="Select project")
-  zellij options --on-force-close quit 
+  zellij -s $zellijname options --on-force-close quit
+  zellij delete-session $zellijname
+
 done
