18623efc04fe575a6ea121ea3e62dba8ebd563e5
sawyer bristol <sawyerbristol@gmail.com>
2025-05-19 18:40:48 -0600

    add settings

--- dots/.config/quitebrowser/config.py
+++ dots/.config/quitebrowser/config.py
@@ -20,15 +20,11 @@ if os.path.exists(config.configdir / "theme.py"):
 
     theme.setup(c, "macchiato", True)
 
-c.tabs.show = "multiple"
-c.tabs.position = "left"
-c.tabs.max_width = -1
-c.tabs.width = "15%"
-
-c.zoom.default = 90
-
 c.tabs.title.format = "{audio}{current_title}"
 c.fonts.web.size.default = 20
+
+c.url.start_pages = "https://search.sawyer.services"
+c.url.default_page = "https://search.sawyer.services"
 
 c.url.searchengines = {
     "DEFAULT": "https://unduck.sawyer.services.com/?q={}",
@@ -40,6 +36,7 @@ c.completion.open_categories = [
     "bookmarks",
     "history",
     "filesystem",
+    # c.window.transparent = True # apparently not needed
 ]
 
 c.auto_save.session = True  # save tabs on quit/restart
@@ -72,9 +69,11 @@ c.colors.webpage.darkmode.policy.images = "never"
 # config.set("colors.webpage.darkmode.enabled", False, "file://*")
 
 # styles, cosmetics
+c.zoom.default = 90
 c.tabs.padding = {"top": 5, "bottom": 5, "left": 9, "right": 9}
+c.tabs.position = "left"
 c.tabs.indicator.width = 0  # no tab indicators
-c.tabs.width = "7%"
+c.tabs.width = "10%"
 
 # fonts
 c.fonts.default_family = []
@@ -85,15 +84,12 @@ c.fonts.web.family.serif = "monospace"
 c.fonts.web.family.standard = "monospace"
 
 # privacy
-config.set("completion.cmd_history_max_items", 0)
-config.set("content.private_browsing", True)
 config.set("content.webgl", False, "*")
 config.set("content.canvas_reading", False)
 config.set("content.geolocation", False)
 config.set("content.webrtc_ip_handling_policy", "default-public-interface-only")
 config.set("content.cookies.accept", "all")
 config.set("content.cookies.store", True)
-# config.set("content.javascript.enabled", False) # tsh keybind to toggle
 
 c.content.blocking.enabled = True
 c.content.blocking.method = "adblock"
