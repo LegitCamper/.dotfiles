34c574221976e2780bfaecba2b039afc050ee060
sawyer bristol <sawyerbristol@gmail.com>
2025-08-18 16:30:09 -0600

    allow zellij to retain projects as sessions

--- dots/.config/zellij/start.sh
+++ dots/.config/zellij/start.sh
@@ -1,10 +1,7 @@
-#!/bin/usr/bash
-
-zellijname=$(shuf -i 1-100000 -n 1)
+#!/usr/bin/bash
 
 while true; do
-  cd ~/projects/$(eza -a ~/projects/ | fzf --margin 30% --header="Select project")
-  zellij -s $zellijname options --on-force-close quit
-  zellij delete-session $zellijname
-
+  project=$(eza -aD ~/projects/ | fzf --margin 30% --header="Select project")
+  cd ~/projects/$project
+  zellij a $project || zellij -s "$project"
 done
