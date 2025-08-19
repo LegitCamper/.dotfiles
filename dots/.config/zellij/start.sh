b8e0ddc1e48419188e59f42f0b618d7051e1d78d
sawyer bristol <sawyerbristol@gmail.com>
2025-08-18 23:50:19 -0600

    enable new project creation from zellij start script
    start in projects dir

--- dots/.config/zellij/start.sh
+++ dots/.config/zellij/start.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/bash
 
 while true; do
-  project=$(eza -aD ~/projects/ | fzf --margin 30% --header="Select project")
-  cd ~/projects/$project
-  zellij a $project || zellij -s "$project"
+  project=$(eza -aD ~/projects/ | awk 'FNR==1  {print "New Project"}1' | fzf --margin 30% --header="Select project")
+  if [ "$project" = "New Project" ]; then
+    clear && cd ~/projects && fish
+  else
+    cd ~/projects/$project
+    zellij a $project || zellij -s "$project"
+  fi
 done
