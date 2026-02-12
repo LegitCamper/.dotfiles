404f36098aa0884101ab5f3af47e754671686fee
sawyer bristol <sawyerbristol@gmail.com>
2026-02-12 15:58:34 -0700

    disable zellij mouse effects and quit on close

--- dots/.config/zellij/start.sh
+++ dots/.config/zellij/start.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/bash
 
 while true; do
-  project=$(eza -aD ~/projects/ | awk 'FNR==1  {print "New Project"}1' | fzf --margin 30% --header="Select project")
+  project=$(eza -aD ~/projects/ | awk 'FNR==1  {print "New Project"}1' | \
+    fzf --margin 30% --header="Select project")
+
   if [ "$project" = "New Project" ]; then
     clear && cd ~/projects && fish
   else
     cd ~/projects/$project
-    zellij a $project || zellij -s "$project"
-    zellij delete-session "$project" --force
+    zellij -s "$project"
   fi
 done
